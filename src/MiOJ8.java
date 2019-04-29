749faa718bf837e803f7ac7634b2436f9787b6a8
XS_Wong <35169154+Pine0@users.noreply.github.com>
2019-04-30 02:12:15 +0800

    试试水

--- src/MiOJ8.java
+++ src/MiOJ8.java
@@ -1,21 +1,31 @@
 import java.util.*;
 
+/**
+ * 题目描述
+ *
+ * 给出一个无序数列，每次只能交换相邻两个元素，求将原数列变成递增数
+ * 列的最少交换次数。 如：数列：2,3,1，交换3和1后变成：2,1,3；交
+ * 换1和2之后变成：1,2,3。总共交换2次。
+ */
 
 public class MiOJ8 {
+
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         String line;
         while (scan.hasNextLine()) {
             line = scan.nextLine().trim();
             // please write your code here
+
+            //将输入的字符串转为数组
             int res = 0, index = 0;
             int[] arr1 = new int[line.length()/2 + 1];
-
             for (String item : line.split(",")) {
                 arr1[index] = Integer.valueOf(item);
                 index++;
             }
 
+            //冒泡计算swap次数
             for (int i = arr1.length - 1; i >= 0; i--) {
                 for (int j = 0; j < i; j++) {
                     if (arr1[j] > arr1[j + 1]) {
@@ -27,8 +37,9 @@ public class MiOJ8 {
                 }
             }
 
-
+            //输出结果
             System.out.println(res);
+
             // System.out.println("answer");
         }
     }
--- src/SimilarQuestions/MiOJ8SimilarQuestions.java
+++ src/SimilarQuestions/MiOJ8SimilarQuestions.java
@@ -2,7 +2,13 @@ package SimilarQuestions;
 
 import java.util.*;
 
-
+/**
+ * 类似题目描述
+ *
+ * 给出一个无序数列，每次只能交换两个元素，求将原数列变成递增数
+ * 列的最少交换次数。 如：数列：4,2,3,1，交换4和1后变成：1,2,
+ * 3,4。总共交换1次。
+ */
 public class MiOJ8SimilarQuestions {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
@@ -10,17 +16,20 @@ public class MiOJ8SimilarQuestions {
         while (scan.hasNextLine()) {
             line = scan.nextLine().trim();
             // please write your code here
+
+            //将输入的字符串转为数组
             int res = 0;
             String[] temp = line.split(",");
             int[] arr1 = new int[temp.length];
-
             for (int i = 0; i < temp.length; i++) {
                 arr1[i] = Integer.valueOf(temp[i]);
             }
 
+            //将arr1排序 并赋值给aar2
             int[] arr2 = Arrays.copyOf(arr1, arr1.length);
             Arrays.sort(arr2);
 
+            //创建一个字典用于存需要交换的键值对，key为需要交换的元素值，value为需要将key放到下标为value的地方
             Map<Integer, Integer> map = new HashMap<>();
             Set<Integer> set = new HashSet<>();
             for (int i = 0; i < arr1.length; i++) {
@@ -29,6 +38,12 @@ public class MiOJ8SimilarQuestions {
                 }
             }
 
+            /**
+             * 遍历这个字典，取出交换集合元素数。
+             * 交换集合举例：
+             * 例如 4,1,2,3 中的交换集合元素数为4，因为这四个元素每一个位置都要变。
+             * 例如 5,2,4,1,3 中的交换集合元素数也为4，因为只有5,4,1,3这四个元素需要改变位置
+             */
             Iterator<Map.Entry<Integer, Integer>> iter = map.entrySet().iterator();
             while (map.size() != 0) {
                 Map.Entry<Integer, Integer> item = iter.next();
@@ -45,7 +60,9 @@ public class MiOJ8SimilarQuestions {
                 set.clear();
             }
 
+            //输出结果
             System.out.println(res);
+
             // System.out.println("answer");
         }
     }
